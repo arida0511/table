8af5cf442369dabcc27a739cfb170393ea13b0be
한현우 <hyunwoo@ip-10-10-10-8.ap-northeast-2.compute.internal>
2022-07-29 23:39:12 +0900

    Bar Graph Example

--- src/component/bar-graph-svg/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as BarGraphSVG } from './index.svelte';
-export { default as BarItemSVG } from './item/index.svelte';
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import { BarGraph, BarItem } from '@src/component/bar-graph';
-	import { BarGraphSVG, BarItemSVG } from '@src/component/bar-graph-svg';
 	let data: number[] = [1, 2, 3, 5, 8];
 	let title: string = 'Hello World';
 	let minV: number;
@@ -8,43 +7,29 @@
 </script>
 
 <div class="root">
-	<BarGraph {data} {title} bind:minV bind:maxV>
-		{#each data as d}
-			<BarItem min={minV} max={maxV} value={d} />
-		{/each}
-	</BarGraph>
+	<h1>Graph Component Example</h1>
+	<div class="graph-field">
+		<BarGraph {data} {title} bind:minV bind:maxV>
+			{#each data as d}
+				<BarItem min={minV} max={maxV} value={d} />
+			{/each}
+		</BarGraph>
+	</div>
 </div>
 
 <style lang="scss">
-	rect {
-		fill: #f00;
-	}
-	circle {
-		fill: none;
-		stroke: #f0f;
-		stroke-dasharray: 3, 3;
-		stroke-width: 3px;
-		stroke-dashoffset: 1;
-		animation: anim 10s;
-	}
-	svg {
+	.root {
+		position: relative;
 		width: 100%;
 		height: 100%;
-	}
-	.root {
-		background: #fff;
-		height: 80vh;
-		border: solid 2px #000;
-		position: relative;
-		.parent {
-			background: #00f;
-			width: 100%;
-			height: 100%;
-			.test1 {
-				width: 100px;
-				height: 100px;
-				background: #000;
-			}
+		padding: 32px;
+		.graph-field {
+			height: 600px;
 		}
+	}
+
+	:global(body, html) {
+		padding: 0;
+		margin: 0;
 	}
 </style>
