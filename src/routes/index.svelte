170f67078151aecdc650dbe881770e02b9d1324a
LAPTOP-C047TOU8\졍 <doeob6@ajou.ac.kr>
2022-08-08 21:50:08 +0900

    0804

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,19 +1,46 @@
 <script lang="ts">
 	import { BarGraph, BarItem } from '@src/component/bar-graph';
+import { update } from 'lodash';
+	import {onMount, onDestroy} from 'svelte';
+	import {updateData ,priceData, type PriceUnit} from '../store/index';
 	let data: number[] = [1, 2, 3, 5, 8];
 	let title: string = 'Hello World';
 	let minV: number;
 	let maxV: number;
+
+	onMount(()=> {
+		updateData(7);
+	});
 </script>
 
 <div class="root">
-	<h1>Graph Component Example</h1>
+	<select>
+		<option>7</option>
+		<option>30</option>
+		<option>90</option>
+	</select>
+	<h1>{$priceData.length}</h1> <!--데이터 크기-->
+	<div>
+		
+		<div class= "table">
+			{#each $priceData as price}
+			<div class="item" style="display: flex">
+				<div id="itm" class="name">{price.denom}</div>
+				<div id="itm" class="timestamp">{price.timestamp}</div>
+				<div id="itm" class="price">{price.price}</div>
+				<div id="itm" class="marketCap">{price.marketCap}</div>
+				<div id="itm" class="dayVolume">{price.dayVolume}</div>
+			</div>
+			{/each}
+		</div>
+	</div>
 	<div class="graph-field">
-		<BarGraph {data} {title} bind:minV bind:maxV>
+		
+		<!--<BarGraph {data} {title} bind:minV bind:maxV>
 			{#each data as d}
 				<BarItem min={minV} max={maxV} value={d} />
 			{/each}
-		</BarGraph>
+		</BarGraph>-->
 	</div>
 </div>
 
@@ -26,6 +53,10 @@
 		.graph-field {
 			height: 600px;
 		}
+	}
+	
+	#itm {
+		width: 200px;
 	}
 
 	:global(body, html) {
